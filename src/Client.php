7be3958d5c5b556ed68dec6d7f500a7e85e2f391
pkowalczyk <11366345+PabloKowalczyk@users.noreply.github.com>
2018-10-20 17:18:39 +0200

    Implement Place Details fetching.

--- src/Client.php
+++ src/Client.php
@@ -4,20 +4,102 @@ declare(strict_types=1);
 
 namespace GPlacesPhp\ApiClient;
 
+use GPlacesPhp\ApiClient\Cache\NullCache;
+use GPlacesPhp\ApiClient\Client\PlaceDetails;
+use GPlacesPhp\ApiClient\Client\Url;
+use GPlacesPhp\ApiClient\Exception\ApiException;
 use GPlacesPhp\ApiClient\Exception\ClientException;
+use Http\Client\HttpClient;
+use Psr\Http\Message\RequestFactoryInterface;
+use Psr\Http\Message\StreamInterface;
+use Psr\SimpleCache\CacheInterface;
 
-final class Client
+final class Client implements ClientInterface
 {
     /** @var string */
     private $key;
+    /** @var HttpClient */
+    private $httpClient;
+    /** @var CacheInterface */
+    private $cache;
+    /** @var RequestFactoryInterface */
+    private $requestFactory;
 
     /** @throws ClientException */
-    public function __construct(string $apiKey)
-    {
+    private function __construct(
+        string $apiKey,
+        HttpClient $httpClient,
+        RequestFactoryInterface $requestFactory,
+        CacheInterface $cache
+    ) {
         if ('' === $apiKey) {
             throw new ClientException('Argument "apiKey" cannot be empty string.');
         }
 
         $this->key = $apiKey;
+        $this->httpClient = $httpClient;
+        $this->cache = $cache;
+        $this->requestFactory = $requestFactory;
+    }
+
+    public static function create(
+        string $apiKey,
+        HttpClient $httpClient,
+        RequestFactoryInterface $requestFactory,
+        CacheInterface $cache = null
+    ): self {
+        return new self(
+            $apiKey,
+            $httpClient,
+            $requestFactory,
+            $cache ?? new NullCache()
+        );
+    }
+
+    public function placeDetails(string $placeId, PlaceDetails\OptionalParameters $parameters = null): PlaceDetails
+    {
+        $url = Url::details(
+            $this->key,
+            $placeId,
+            $parameters ?? PlaceDetails\OptionalParameters::fromArray([])
+        );
+        $request = $this->requestFactory
+            ->createRequest('GET', $url->toString());
+        $isCached = $this->cache
+            ->has($url->toHash());
+        $placeDetailsData = null;
+
+        if (!$isCached) {
+            $response = $this->httpClient
+                ->sendRequest($request);
+            $responseData = $this->parseJsonResponse($response->getBody());
+            $responseStatus = $responseData['status'] ?? '';
+
+            if ('OK' !== $responseStatus) {
+                throw ApiException::create($responseStatus, $responseData['error_message'] ?? '');
+            }
+
+            $placeDetailsData = $responseData['result'] ?? [];
+
+            $this->cache
+                ->set($url->toHash(), $placeDetailsData);
+        } else {
+            $placeDetailsData = $this->cache
+                ->get($url->toHash());
+        }
+
+        return PlaceDetails::fromArray($placeDetailsData);
+    }
+
+    private function parseJsonResponse(StreamInterface $stream): array
+    {
+        $data = \json_decode((string) $stream, true);
+
+        if (\JSON_ERROR_NONE !== \json_last_error()) {
+            $jsonErrorMessage = \json_last_error_msg();
+            throw new ClientException("Unable to parse response, error message: '{$jsonErrorMessage}'.");
+        }
+
+        return $data;
     }
 }
--- tests/Unit/ClientTest.php
+++ tests/Unit/ClientTest.php
@@ -6,16 +6,168 @@ namespace GPlacesPhp\ApiClient\Tests\Unit;
 
 use GPlacesPhp\ApiClient\Client;
 use GPlacesPhp\ApiClient\Exception\ClientException;
+use GPlacesPhp\ApiClient\Tests\TestCase\Cache\CacheSpy;
+use GPlacesPhp\ApiClient\Tests\TestCase\FixtureLoader\FixtureLoader;
+use Http\Client\HttpClient;
 use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\RequestFactoryInterface;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\StreamInterface;
+use Psr\Http\Message\UriInterface;
 
 final class ClientTest extends TestCase
 {
     /** @test */
-    public function clientRequiresApiKey(): void
+    public function client_requires_api_key(): void
     {
         $this->expectException(ClientException::class);
         $this->expectExceptionMessage('Argument "apiKey" cannot be empty string.');
 
-        new Client('');
+        Client::create(
+            '',
+            $this->createDummyClient(),
+            $this->createDummyRequestFactory()
+        );
+    }
+
+    /** @test */
+    public function details_are_read_from_cache_if_cached(): void
+    {
+        $fixtureLoader = new FixtureLoader();
+        $detailsData = $fixtureLoader->getJson('details-response')['result'] ?? [];
+        $cacheMock = new CacheSpy($detailsData);
+
+        $client = Client::create(
+            'some-api-key',
+            $this->createDummyClient(),
+            $this->createDummyRequestFactory(),
+            $cacheMock
+        );
+        $client->placeDetails('some-place-id');
+
+        $this->assertSame(1, $cacheMock->getCall);
+    }
+
+    private function createDummyClient(): HttpClient
+    {
+        return new class() implements HttpClient {
+            /**
+             * {@inheritdoc}
+             */
+            public function sendRequest(RequestInterface $request)
+            {
+            }
+        };
+    }
+
+    private function createDummyRequestFactory(): RequestFactoryInterface
+    {
+        $request = $this->createDummyRequest();
+
+        return new class($request) implements RequestFactoryInterface {
+            /** @var RequestInterface */
+            private $request;
+
+            public function __construct(RequestInterface $request)
+            {
+                $this->request = $request;
+            }
+
+            /** {@inheritdoc} */
+            public function createRequest(string $method, $uri): RequestInterface
+            {
+                return $this->request;
+            }
+        };
+    }
+
+    private function createDummyRequest(): RequestInterface
+    {
+        return new class() implements RequestInterface {
+            /** {@inheritdoc} */
+            public function getProtocolVersion()
+            {
+            }
+
+            /** {@inheritdoc} */
+            public function withProtocolVersion($version)
+            {
+            }
+
+            /** {@inheritdoc} */
+            public function getHeaders()
+            {
+            }
+
+            /** {@inheritdoc} */
+            public function hasHeader($name)
+            {
+            }
+
+            /** {@inheritdoc} */
+            public function getHeader($name)
+            {
+            }
+
+            /** {@inheritdoc} */
+            public function getHeaderLine($name)
+            {
+            }
+
+            /** {@inheritdoc} */
+            public function withHeader($name, $value)
+            {
+            }
+
+            /** {@inheritdoc} */
+            public function withAddedHeader($name, $value)
+            {
+            }
+
+            /** {@inheritdoc} */
+            public function withoutHeader($name)
+            {
+            }
+
+            /** {@inheritdoc} */
+            public function getBody()
+            {
+            }
+
+            /** {@inheritdoc} */
+            public function withBody(StreamInterface $body)
+            {
+            }
+
+            /** {@inheritdoc} */
+            public function getRequestTarget()
+            {
+            }
+
+            /** {@inheritdoc} */
+            public function withRequestTarget($requestTarget)
+            {
+            }
+
+            /** {@inheritdoc} */
+            public function getMethod()
+            {
+            }
+
+            /** {@inheritdoc} */
+            public function withMethod($method)
+            {
+            }
+
+            /** {@inheritdoc} */
+            public function getUri()
+            {
+            }
+
+            /** {@inheritdoc} */
+            public function withUri(UriInterface $uri, $preserveHost = false)
+            {
+            }
+        };
     }
 }
