0a622fc8aeb64ef90b6f84b49905079bc158b2da
pkowalczyk <11366345+PabloKowalczyk@users.noreply.github.com>
2018-10-20 14:39:54 +0200

    Add LocationMother to fabricate Locations.

---
+++ tests/TestCase/Mother/LocationMother.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GPlacesPhp\ApiClient\Tests\TestCase\Mother;
+
+use GPlacesPhp\ApiClient\Client\PlaceDetails\Geometry\Location;
+
+final class LocationMother
+{
+    public static function random(): Location
+    {
+        $randomLat = \random_int(-89, 89) + (\mt_rand() / \mt_getrandmax());
+        $randomLng = \random_int(-179, 179) + (\mt_rand() / \mt_getrandmax());
+
+        return self::withValues($randomLat, $randomLng);
+    }
+
+    public static function withValues(float $lat, float $lng): Location
+    {
+        return Location::fromArray(
+            [
+                'lat' => $lat,
+                'lng' => $lng,
+            ]
+        );
+    }
+}
