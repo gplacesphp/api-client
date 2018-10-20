5d668dbf639811f42fb7f5742f7f5b9ff0f8dbdb
pkowalczyk <11366345+PabloKowalczyk@users.noreply.github.com>
2018-10-20 17:18:20 +0200

    Add FixtureLoader.

---
+++ tests/TestCase/FixtureLoader/FixtureLoader.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GPlacesPhp\ApiClient\Tests\TestCase\FixtureLoader;
+
+final class FixtureLoader
+{
+    /** @var string */
+    private $fixturesDir;
+
+    public function getJson(string $name): array
+    {
+        $fixturesDir = $this->fixturesDir();
+        $fixtureFile = "{$fixturesDir}/{$name}.json";
+
+        if (!\file_exists($fixtureFile)) {
+            throw new \RuntimeException("Fixture file '{$fixtureFile}' not found.");
+        }
+
+        return \json_decode(\file_get_contents($fixtureFile), true);
+    }
+
+    private function fixturesDir(): string
+    {
+        if (null === $this->fixturesDir) {
+            $r = new \ReflectionObject($this);
+            $dir = $rootDir = \dirname($r->getFileName());
+            while (!\file_exists($dir . '/fixtures')) {
+                if ($dir === \dirname($dir)) {
+                    return $this->fixturesDir = "{$rootDir}/fixtures";
+                }
+                $dir = \dirname($dir);
+            }
+            $this->fixturesDir = "{$dir}/fixtures";
+        }
+
+        return $this->fixturesDir;
+    }
+}
