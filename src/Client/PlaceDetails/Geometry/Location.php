cbf6a2ee4226f217e77654f5da15c576cfcd45fc
pkowalczyk <11366345+PabloKowalczyk@users.noreply.github.com>
2018-10-20 14:05:58 +0200

    Add Location VO.

---
+++ src/Client/PlaceDetails/Geometry/Location.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GPlacesPhp\ApiClient\Client\PlaceDetails\Geometry;
+
+final class Location
+{
+    /** @var float */
+    private $latitude;
+    /** @var float */
+    private $longitude;
+
+    private function __construct(float $latitude, float $longitude)
+    {
+        $this->latitude = $latitude;
+        $this->longitude = $longitude;
+    }
+
+    public function latitude(): float
+    {
+        return $this->latitude;
+    }
+
+    public function longitude(): float
+    {
+        return $this->longitude;
+    }
+
+    public static function fromArray(array $data): self
+    {
+        return new self($data['lat'] ?? 0.0, $data['lng'] ?? 0.0);
+    }
+}
---
+++ tests/Unit/Client/PlaceDetails/Geometry/LocationTest.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GPlacesPhp\ApiClient\Tests\Unit\Client\PlaceDetails\Geometry;
+
+use GPlacesPhp\ApiClient\Client\PlaceDetails\Geometry\Location;
+use PHPUnit\Framework\TestCase;
+
+final class LocationTest extends TestCase
+{
+    /** @test */
+    public function location_can_be_created_from_array(): void
+    {
+        $lat = 21.43242;
+        $lng = 52.43243;
+        $location = Location::fromArray(
+            [
+                'lat' => $lat,
+                'lng' => $lng,
+            ]
+        );
+
+        $this->assertSame($lat, $location->latitude());
+        $this->assertSame($lng, $location->longitude());
+    }
+}
