ceefe7eed2cc5aa3f3c6cfcb25c35cff26883c88
pkowalczyk <11366345+PabloKowalczyk@users.noreply.github.com>
2018-10-20 17:34:57 +0200

    Fix issues reported by PHPStan.

--- tests/TestCase/FixtureLoader/FixtureLoader.php
+++ tests/TestCase/FixtureLoader/FixtureLoader.php
@@ -18,14 +18,14 @@ final class FixtureLoader
             throw new \RuntimeException("Fixture file '{$fixtureFile}' not found.");
         }
 
-        return \json_decode(\file_get_contents($fixtureFile), true);
+        return \json_decode(\file_get_contents($fixtureFile) ?: '[]', true);
     }
 
     private function fixturesDir(): string
     {
         if (null === $this->fixturesDir) {
             $r = new \ReflectionObject($this);
-            $dir = $rootDir = \dirname($r->getFileName());
+            $dir = $rootDir = \dirname($r->getFileName() ?: '');
             while (!\file_exists($dir . '/fixtures')) {
                 if ($dir === \dirname($dir)) {
                     return $this->fixturesDir = "{$rootDir}/fixtures";
