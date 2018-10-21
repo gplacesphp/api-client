79d542c8ccbb6610c4f7c4226c6a90e9c18cb7e8
pkowalczyk <11366345+PabloKowalczyk@users.noreply.github.com>
2018-10-21 15:06:39 +0200

    Add input type related constants.

--- src/ClientInterface.php
+++ src/ClientInterface.php
@@ -8,5 +8,9 @@ use GPlacesPhp\ApiClient\Client\PlaceDetails;
 
 interface ClientInterface
 {
+    public const INPUT_TYPE_PHONE = 'phonenumber';
+    public const INPUT_TYPE_TEXT = 'textquery';
+    public const INPUT_TYPES = [self::INPUT_TYPE_TEXT, self::INPUT_TYPE_PHONE];
+
     public function placeDetails(string $placeId, PlaceDetails\OptionalParameters $parameters = null): PlaceDetails;
 }
