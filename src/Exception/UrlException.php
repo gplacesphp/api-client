5d9dea3019a50b502adaa4119de3efcfe33d3cd7
pkowalczyk <11366345+PabloKowalczyk@users.noreply.github.com>
2018-10-21 17:59:11 +0200

    Add findPlace method to Url.

--- src/Exception/UrlException.php
+++ src/Exception/UrlException.php
@@ -15,4 +15,21 @@ class UrlException extends ClientException
     {
         return new self('Argument "placeId" is required and cannot be empty.');
     }
+
+    public static function emptyInput(): self
+    {
+        return new self('Argument "input" is required and cannot be empty.');
+    }
+
+    public static function emptyInputType(): self
+    {
+        return new self('Argument "inputType" is required and cannot be empty.');
+    }
+
+    public static function unsupportedInputType(string $inputType, array $inputTypes): self
+    {
+        $inputTypesString = \implode("', '", $inputTypes);
+
+        return new self("Input type '{$inputType}' is not supported, try one of these: '{$inputTypesString}'.");
+    }
 }
