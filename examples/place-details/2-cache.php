9b2c2cc96c4bd585b12c7a7cf9f12f57cf7201c2
PabloKowalczyk <11366345+PabloKowalczyk@users.noreply.github.com>
2018-11-18 11:01:26 +0100

    Update examples. (#8)

--- examples/place-details/2-cache.php
+++ examples/place-details/2-cache.php
@@ -3,7 +3,8 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $apiKey = '<YOUR_API_KEY>';
-$httpClient = new \Http\Client\Curl\Client();
+$guzzle = new \GuzzleHttp\Client();
+$httpClient = new \Http\Adapter\Guzzle6\Client($guzzle);
 $requestFactory = new \Zend\Diactoros\RequestFactory();
 $filesystemCache = new \Symfony\Component\Cache\Simple\FilesystemCache();
 
